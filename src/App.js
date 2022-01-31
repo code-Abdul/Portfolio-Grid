f466f98c78b15387432ef9ed0b3b7b4f09a04292
Abdul Saboor Ahmed <abahmed@firstam.com>
2022-01-31 11:56:47 -0600

    create portfolio grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import "./App.css";
 
 function App() {
+  const [portfolioData, setPortfolioData] = useState([]);
+  const [cardIndex, setCardIndex] = useState();
+
+  useEffect(() => {
+    getPortfolioData();
+  }, []);
+
+  async function getPortfolioData() {
+    const response = await fetch("https://api.imgflip.com/get_memes", {
+      method: "GET",
+    });
+    response.json().then((res) => {
+      setPortfolioData(res.data);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Portfolio Grid 4</h1>
+        <p>This grid shows the items pages in a popup</p>
       </header>
+      <Box>
+        <Grid container spacing={1}>
+          {portfolioData?.memes?.map((meme, index) => (
+            <Grid item xs={6} sm={4} md={3} key={index}>
+              <Card
+                sx={{ maxWidth: 345 }}
+                onMouseOver={() => setCardIndex(index)}
+              >
+                {cardIndex !== index ? (
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="240"
+                      image={meme?.url}
+                      alt={meme?.name}
+                    />
+                  </CardActionArea>
+                ) : (
+                  <CardContent>
+                    <Typography>
+                      <h5>{meme?.name}</h5>
+                    </Typography>
+                  </CardContent>
+                )}
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </div>
   );
 }
